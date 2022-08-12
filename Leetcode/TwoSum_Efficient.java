345d9f34350de2e158141d8e527ffd48f05c7561
tech009 <in_aman_arya@outlook.com>
2022-08-12 09:24:12 +0000

    TwoSum

---
+++ Leetcode/TwoSum_Efficient.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int res[] = new int[2];
+        HashMap <Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length; i++){
+            map.put(nums[i], i);
+                }
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey((target-nums[i]))){
+                res[0] = i;
+                res[1] = map.get((target-nums[i]));
+                break;
+                }
+            }
+        return res;
+    }
+}
