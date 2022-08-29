a3e966c1429515d56ee4650138f619bb6945c654
tech009 <in_aman_arya@outlook.com>
2022-08-29 12:33:12 +0000

    Happy Numbers

---
+++ Leetcode/HappyNumber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while(n!=1){
+            int sum = 0;
+            while(n!=0){
+                sum+=((n%10)*(n%10));
+                n/=10;
+            }
+            if(set.contains(sum))
+                return false;
+            
+            set.add(sum);
+            n = sum;
+        }
+        return true;
+    }
+}
